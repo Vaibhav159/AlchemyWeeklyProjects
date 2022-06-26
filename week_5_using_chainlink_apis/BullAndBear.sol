69e33c74718c052b017607b037820b2425df9255
Vaibhav Lodha <vaibhav.lodha@auctm.com>
2022-06-26 21:00:13 +0530

    adding of random function

--- week_5_using_chainlink_apis/BullAndBear.sol
+++ week_5_using_chainlink_apis/BullAndBear.sol
@@ -9,8 +9,54 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatibleInterface {
+// contract address -> 0x74A8AEd6402bAF851B39cA33dD0373921e516516
+
+contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatibleInterface, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Map of request IDs to the corresponding sender.
+    mapping(uint256 => address) public requestIdToSender;
+
+    // Map of request IDs to the corresponding random number.
+    mapping(uint256 => uint256) public requestIdToRandomNumber;
+
+    // constant
+    uint256 public constant RANDOM_RANGE = 4;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // Rinkeby coordinator. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords =  1;
+
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    address s_owner;
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -34,7 +80,7 @@ contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Kee
 
     event TokensUpdated(string marketTrend);
 
-    constructor(uint updateInterval, address _priceFeed) ERC721("BullAndBear", "BAB") {
+    constructor(uint updateInterval, address _priceFeed, uint64 subscriptionId) ERC721("BullAndBear", "BAB") VRFConsumerBaseV2(vrfCoordinator) {
         interval = updateInterval;
         lastTimestamp = block.timestamp;
 
@@ -43,6 +89,10 @@ contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Kee
         // or the MockPriceFeed Contract
         priceFeed = AggregatorV3Interface(_priceFeed);
         currentPrice = getLatestPrice();
+
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
     }
 
     function safeMint(address to) public onlyOwner {
@@ -100,11 +150,13 @@ contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Kee
     function updateAllTokenUris(string memory trend) internal{
 
         string memory newTrendUri;
+        uint256 randon_number = getRandomNumber();
+
         if (compareStrings("bear", trend)){
-            newTrendUri = bearUrisIpfs[1];
+            newTrendUri = bearUrisIpfs[randon_number];
         }
         else{
-            newTrendUri = bullUrisIpfs[1];
+            newTrendUri = bullUrisIpfs[randon_number];
         }
 
         for(uint i=0;i<_tokenIdCounter.current();i++){
@@ -125,6 +177,38 @@ contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Kee
 
     function setPriceFeed(address newFeedAddress) public onlyOwner{
         priceFeed = AggregatorV3Interface(newFeedAddress);
+    }
+
+      // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() internal onlyOwner returns (uint256) {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+        keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+        );
+
+        requestIdToSender[s_requestId] = msg.sender;
+
+        return s_requestId;
+    }
+    
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+        requestIdToRandomNumber[requestId] = (randomWords[0] % RANDOM_RANGE);
+    }
+    
+
+    // Request random words from the VRF coordinator and get the random number.
+    function getRandomNumber() internal returns (uint256) {
+        uint256 requestId = requestRandomWords();
+        uint256 randomNumber = requestIdToRandomNumber[requestId];
+        return randomNumber;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
